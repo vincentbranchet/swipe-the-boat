b3d1ad888a9f8b8a821f0624a80bdfcf9e279e44
Vincent Branchet <vince.branchet@gmail.com>
2022-11-09 08:55:33 +0100

    Player / fixed boat size decreasing bug

--- assets/PlayerCharacter.js
+++ assets/PlayerCharacter.js
@@ -4,18 +4,30 @@ export default class PlayerCharacter {
         this.boat = [];
         this.characters = [];
         this.maxSpeed = 100;
+        this.debutWidth = 40;
+        this.debutHeight = 40;
     }
 
     init() {
         this.boat = this.scene.physics.add.group();
+        this.characters = this.scene.physics.add.group();
+
         const raft = this.scene.physics.add.sprite(0, -100, 'beach', 20).refreshBody();
+        
+        raft.width = this.debutWidth;
+        raft.height = this.debutHeight;
+        raft.displayWidth = this.debutWidth;
+        raft.displayHeight = this.debutHeight;
+        
         this.boat.add(raft);
         this.boat.setDepth(1);
         this.boat.getChildren().forEach(b => b.setDrag(50));
         this.boat.getChildren().forEach(b => b.body.setMaxSpeed(this.maxSpeed));
 
-        this.characters = this.scene.physics.add.group();
+        console.log(`Boat was created with size (${raft.width}, ${raft.height}).`);
+        
         const adam = this.scene.physics.add.sprite(0, -100, 'characters', 12).refreshBody();
+        
         this.characters.add(adam);
         this.characters.setDepth(10);
     }
@@ -26,15 +38,21 @@ export default class PlayerCharacter {
      */
     handleTouchedDebris() {
         console.log('Player has touched a debris.');
+        
         if(this.body.active) {
             const player = this.chunk.scene.player;
             const boat = player.boat.getChildren()[0];
             const newSize = {width: Math.round(boat.width * 1.1), height: Math.round(boat.height * 1.1)};
             const currentZoom = this.chunk.scene.cameras.main.zoom;
+
+            console.log(`Player current size is ${boat.width} x ${boat.height}`);
             
             player.maxSpeed = player.maxSpeed + 20;
             boat.width = boat.displayWidth = newSize.width;
             boat.height = boat.displayHeight = newSize.height;
+            boat.body.setMaxSpeed(player.maxSpeed);
+
+            console.log(`Changed player size to ${boat.width} x ${boat.height}`);
 
             this.chunk.scene.score++;
             this.chunk.scene.cameras.main.setZoom(currentZoom - 0.05);
